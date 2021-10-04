9fcad680e461b1502a4bc1f95cdf38d0561cd058
jisu Choi <pray92@users.noreply.github.com>
2021-10-04 10:13:12 +0900

    회원 등록

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @Slf4j      // Logger Annotation
 public class HomeController {
